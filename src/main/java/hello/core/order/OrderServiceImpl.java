5bcd557dba60c9afcd63b0d48bbaef15476b1728
POIUY <bumcheol1@gmail.com>
2026-02-01 20:13:12 +0900

    생성자 주입 선택
    생성자 주입을 사용할 때만 final 사용가능 - 불변
    수정자 주입을 사용하면 final x
    생성자 + 수정자 동시 사용 가능
    단! 필드 주입은 사용하지 마라!!!

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,43 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+//    @Autowired // @Autowired(required = false) - 수정자(setter)의 특징: 선택
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+//
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    @Autowired // 생성자가 1개일 경우 생략 가능
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository; // MemoryMemberRepository
+        this.discountPolicy = discountPolicy; // FixDiscountPolicy
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
