3c760bc7e4424e2c889f0ef9024981ffcb35dd78
POIUY <bumcheol1@gmail.com>
2026-02-06 21:14:12 +0900

    싱글톤 빈과 사용시 Provider로 해결방법
    사용할 때마다 새로운 프로토타입 빈 인스턴스를 생성하고 싶을 경우
    - ObjectProvider, Provider를 사용해 DL 기능만 제공
    - 스프링이 아닌 외부 컨테이너에서 사용이 필요할 경우 java 표준인 Provider를 사용해야함(JSR-330)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,7 +2,9 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -35,36 +37,18 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입 x01
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider; // ObjectProvider<PrototypeBean> -> getObject()
 
         public int logic() {
-            prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
-        }
-    }
-
-    @Scope("singleton")
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입 x02
-
-        @Autowired
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // 프로토타입 빈을 스프링 컨테이너에서 찾아서 반환해줌. = DL(Dependency Lookup)
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
